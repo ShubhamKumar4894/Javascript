5d90c10d7689217bd732cd37436b08e4675d63b6
Shubham Kumar <146630694+ShubhamKumar4894@users.noreply.github.com>
2024-09-18 18:12:12 +0000

    maps

--- condition.js
+++ condition.js
@@ -17,9 +17,26 @@
 // }
 
 //Nullish coalescing operator (??)
-let val1= null??7;
-console.log(val1)
-let val2= undefined??7;
-console.log(val2)
-let val3= 0??7;
-console.log(val3)
+// let val1= null??7;
+// console.log(val1)
+// let val2= undefined??7;
+// console.log(val2)
+// let val3= 0??7;
+// console.log(val3)
+const arr=[1,2,3,4]
+// for(let i=0;i<4;i++){
+//     console.log("shubham")
+// }
+// for(const num of arr){
+//     console.log(num)
+// }
+
+const map1= new Map();
+map1.set(1,"shub");
+map1.set(2,"ank");
+map1.set(3,"pawan");
+console.log(map1.get(1));
+
+for(const [key,val] of map1){
+    console.log(key ,"-", val)
+}
