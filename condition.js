7adb6a7138bafcef4467d8cf67dbcf6a9c716994
Shubham Kumar <146630694+ShubhamKumar4894@users.noreply.github.com>
2024-09-18 17:08:09 +0000

    condition

---
+++ condition.js
@@ -0,0 +1,25 @@
+// const month=12;
+// switch(month){
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     default:
+//         console.log("any")
+// }
+
+//Nullish coalescing operator (??)
+let val1= null??7;
+console.log(val1)
+let val2= undefined??7;
+console.log(val2)
+let val3= 0??7;
+console.log(val3)
